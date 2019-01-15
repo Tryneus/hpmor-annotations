db32672e91ee73fc38f2e2cb23f280f9101e421d
Grey <grey@vex.local>
2019-01-14 22:26:40 -0500

    add script to download corpus of chapters for testing

---
+++ script/downloadChapters.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const scrape = require('website-scraper');
+const path = require('path');
+
+async function downloadChapters() {
+  const directory = path.join(__dirname, '../chapters');
+  const urls = Array.from(Array(122).keys()).map((i) =>
+    ({url: `https://www.hpmor.com/chapter/${i + 1}`, filename: `${i + 1}.html`}),
+  );
+
+  try {
+    await scrape({urls, directory, requestConcurrency: 1});
+  } catch (e) {
+    console.log('Failed to download chapters:', e.message);
+  }
+}
+
+downloadChapters();
