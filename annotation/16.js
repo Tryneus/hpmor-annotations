08c06d0146c6edbf167f423d5acd11ab8bf54415
Grey <grey@vex.local>
2019-01-27 03:22:39 -0500

    a couple more chapters

--- annotation/16.js
+++ annotation/16.js
@@ -1,79 +1,77 @@
-```
+module.exports = [
+  {
+    tags: ['reference'],
+    text: `
 The enemy's gate is Rowling.
-```
-
-* note
-* reference
-
+    `,
+    note: `
 A reference to "The enemy's gate is down", an insight from Ender's
 Game that in zero-gravity you can orient yourself whichever way you want.
 (Probably meant to be an example of lateral thinking.)
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['foreshadowing', 'spoiler'],
+    text: `
 I'm not a psychopath, I'm just very creative.
-```
-
-* title-quote
-
+    `,
+    note: `
 This is quote from Harry later in this chapter, and relates to the
 theme of the chapter.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['foreshadowing', 'departure', 'spoiler'],
+    text: `
 akin to a major univeristy classroom
-```
-
-* original
-* foreshadowing
-
+    `,
+    note: `
 The Defence class in general is a large departure from the original
 series, but to portray the classroom as such shows that Quirrell is
 not as antiquated as the rest of the wizarding world, and is open
 to taking ideas from Muggles.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 Euclidean
-```
-
-* reference
-* link
-
+    `,
+    note: `
 https://en.wikipedia.org/wiki/Euclidean_geometry
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['speculation', 'background'],
+    text: `
 Now what does that remind me of...?
-```
-
-* subtle
-
+    `,
+    note: `
 This may be referring to Harry's opinions of most of his teachers
 before he began being home-schooled.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler'],
+    text: `
 Harry didn't particularly feel like approaching Quirrell anyway.
-```
-
-* spoiler
-
+    `,
+    note: `
 **sense of doom**
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['original', 'background']
+    text: `
 oh my god you have minions
-```
-
-* original
-
+    `,
+    note: `
 This is a bit of a prod at the source material, in which Draco is
 first portrayed the stereotypical villain with minions in his own
 class.  Here, it is explained how such a thing happens, which only
@@ -81,197 +79,184 @@ serves to make it seem more ridiculous.  The minions themselves,
 Crabbe and Goyle, hamfistedly employ every stereotypical minion action
 available to them, perhaps an explanation of how stupid the characters
 were in the original series.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler'],
+    text: `
 poised in a suspiciously balanced-looking stance.
-```
-
-* spoiler
-
+    `,
+    note: `
 Goyle knows martial arts.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler'],
+    text: `
 "You couldn't get any further away from the teacher if you tried."
-```
-
-* spoiler
-
+    `,
+    note: `
 **sense of doom**
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler', 'foreshadowing'],
+    text: `
 Harry was taken by surprise, both by the sudden appearance of
 Professor Quirrell's face and by the resemblance to Muggle television.
-```
-
-* subtle
-
+    `,
+    note: `
 This is one of the first hints that Quirrell knows a surprising amount
 about Muggle technology -- enough to have clever ideas related to it.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['foreshadowing', 'spoiler'],
+    text: `
 Most fearsome of all, the troll is unique among magical creatures in
 continuously maintaining a form of Transfiguration on itself - it is
 always transforming into its own body.
-```
-
-* foreshadowing
-* spoiler
-
+    `,
+    note: `
 This comes into play later.  Because of this property, mountain trolls
 can be transfigured into something small, smuggled somewhere, and then
-untransfigured (normally this would kill the creature).  (108/`I wore
-the mountain troll as a false tooth`)
+untransfigured (normally this would kill the creature).  {108/I wore
+the mountain troll as a false tooth}
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler', 'foreshadowing'],
+    text: `
 Likewise if you are facing the second most perfect killing machine, a
 Dementor.
-```
-
-* spoiler
-* subtle
-
+    `,
+    note: `
 This is the first hint about the nature of Dementors in HPMOR -- the
 killing curse doesn't work on them, so by Quirrell's logic they can't
 have brains, and yet they're capable of complicated actions.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler'],
+    text: `
 No, there is exactly one monster which can threaten you once you are
 fully grown
-```
-
-* spoiler
-* subtle
-
+    `,
+    note: `
 Note that he does not say these monsters can't threaten you *before*
 you are fully grown.  And, in fact, a troll later kills Hermione.
-(89/`Hermione suddenly convulsed`)
+{89/Hermione suddenly convulsed}
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler', 'foreshadowing'],
+    text: `
 I was in Slytherin and I am offering to formulate a cunning plot on
 your behalf
-```
-
-* spoiler
-* subtle
-* foreshadowing
-
+    `,
+    note: `
 Professor Quirrell was in Ravenclaw in both HPMOR and the original
 timeline.  This is Voldemort pretending to mess up and leak that he is
 not professor Quirrell.  Later it comes out that his plan was to
 pretend to be David Munroe pretending to be professor Quirrell.
-(84/`But I shall tell a story`).
+{84/But I shall tell a story}.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler', 'foreshadowing'],
+    text: `
 That would be Harry.
-```
-
-* foreshadowing
-* spoiler
-
+    `,
+    note: `
 Not incorrect, but maybe a bit misleading.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 He wished the target would dodge like the little spheres that Ben
 Kenobi had used for training Luke
-```
-
-* reference
-
+    `,
+    note: `
 This is a reference to Star Wars: Episode VI where Luke Skywalker
 trains to use a lightsaber by deflecting shots from a flying spherical
 drone, under the supervision of Ben Kenobi.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['foreshadowing', 'spoiler'],
+    text: `
 "No," said Hermione Granger.
-```
-
-* foreshadowing
-* spoiler
-
+    `,
+    note: `
 This scene sets the stage for a major theme throughout the rest of the
 series.  Quirrell is a corrupting force, constantly pushing Harry to
 bend and break rules for seemingly good reasons.  Meanwhile, Hermione
 is a force for good, trying to save Harry from his dark side and
 reminding him that some things cannot be compromised.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler', 'foreshadowing'],
+    text: `
 Demonstrate his supposed immunity to the Killing Curse? Surely
 Professor Quirrell was too smart for that...
-```
-
-* spoiler
-* subtle
-* foreshadoing
-
+    `,
+    note: `
 Professor Quirrell is, in fact, intensely aware of the fact that Harry
 is not immune to the killing curse.
+    `,
+  },
 
----
-
-```
+  {
+    // TODO: spoiler and non-spoiler in single annotation
+    tags: ['original', 'TODO'],
+    text: `
 "My wand can be pushed into an enemy's brain through their eye socket
-```
-
-* original
-
+    `,
+    note: `
 In the original series, Harry sticks a wand up a troll's nose during a
 fight.
 
----
-
-```
-"My wand can be pushed into an enemy's brain through their eye socket
-```
-
-* foreshadowing
-
+Spoiler:
 In HPMOR, Harry kills the troll he fights by sticking his wand into
 its brain through its eye socket and transfiguring a cross-section of
-it into sulfuric acid (89/`His wand jammed through the troll's left
-eye`).
+it into sulfuric acid {89/His wand jammed through the troll's left
+eye}.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler'],
+    text: `
 Harry Potter has it, which is how he could stare down five older
 Slytherins.
-```
-
-* spoiler
-
+    `,
+    note: `
 **mysterious dark side**
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler'],
+    text: `
 WRONG
 DON'T
 BAD IDEA
-```
-
-* spoiler
-
+    `,
+    note: `
 **sense of doom**
-
+    `,
+  },
+];
