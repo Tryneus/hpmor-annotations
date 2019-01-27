00d13e223a0d4ed61a867093bdf153dc350eae45
Grey <grey@vex.local>
2019-01-27 00:08:27 -0500

    three more chapters

---
+++ annotation/10.js
@@ -0,0 +1,234 @@
+module.exports = [
+  {
+    tags: ['reference'],
+    text: `
+All your base are still belong to Rowling.
+    `,
+    note: `
+A reference to All Your Base Are Belong To Us, and also the author's
+note from the previous chapter {9/All your base}.
+
+https://en.wikipedia.org/wiki/All_your_base_are_belong_to_us
+    `,
+  },
+
+  {
+    tags: ['reference'],
+    text: `
+And now you will sit through the Sorting Hat singing its version of
+Evanescence's "My Immortal", which has never happened before.
+
+just kidding
+    `,
+    note: `
+A common meme is that bad fanfiction has to include at least one song
+by a band that highschool kids liked a lot at the time it was written.
+    `,
+  },
+
+  {
+    tags: ['reference'],
+    text: `
+Its song had implied so: Oh, I'm the Sorting Hat and I'm okay, I sleep
+all year and I work one day...
+    `,
+    note: `
+This is not the actual sorting hat song, but instead it is a reference
+to The Lumberjack Song by Monty Python:
+https://www.youtube.com/watch?v=QgaRd4d8hOY
+    `,
+  },
+
+  {
+    tags: ['consequence', 'spoiler'],
+    text: `
+can you tell me anything about how to rediscover the lost magics that created you?
+    `,
+    note: `
+Harry is worried by his discussion with Draco (7/`Our powers have
+grown weaker`) that magic may be draining from the world, so he is
+looking for a way to stem the tide.  This is not far from how
+Voldemort rediscovered lost magic from the basilisk guarding the
+Chamber of Secrets (49/`He-Who-Must-Not-Be-Named began his climb to
+power`), but it is unlikely the Sorting Hat could pass much knowledge
+to Harry due to the **Interdict of Merlin**.
+    `,
+  },
+
+  {
+    tags: ['reference'],
+    text: `
+Godel, Escher, Bach.
+    `,
+    note: `
+A book about recursion/self-reference in math, art, and music.  Very
+accessible and widely read within a certain subset of the college-aged
+population.
+
+https://en.wikipedia.org/wiki/G%C3%B6del,_Escher,_Bach
+    `,
+  },
+
+  {
+    tags: ['background', 'original'],
+    text: `
+And don't meet anyone's eyes while you're thinking about this
+later. Some wizards can read your thoughts if you do.
+    `,
+    note: `
+This is a reference to Legilimency, which plays a large role in HPMOR.
+    `,
+  },
+
+  {
+    tags: ['spoiler'],
+    text: `
+I can tell you that there is definitely nothing like a ghost - mind,
+intelligence, memory, personality, or feelings - in your
+scar. Otherwise it would be participating in this conversation, being
+under my brim.
+    `,
+    note: `
+The intelligence/personality/etc. of the dark lord *is* participating
+in this conversation, since it's just rolled into Harry.  See the
+topic page on his **mysterious dark side**.
+    `,
+  },
+
+  {
+    tags: ['spoiler'],
+    text: `
+I am not Dark Lord material!
+
+"Yes, you are. You really, really are."
+    `,
+    note: `
+Considering Harry's **mysterious dark side** comes from a Dark Lord,
+this is a really big clue.
+    `,
+  },
+
+  {
+    tags: ['consequence', 'TODO'],
+    text: `
+"When I spoke of your anger, you remembered how Professor McGonagall
+told you that she sometimes saw something inside you that didn't seem
+to come from a loving family. You thought of how Hermione, after you
+returned from helping Neville, told you that you had seemed 'scary'."
+    `,
+    note: `
+TODO: backref
+    `,
+  },
+
+  {
+    tags: ['spoiler', 'consequence', 'TODO'],
+    text: `
+No, what you remembered was how you considered lining up all the blood
+purists and guillotining them.
+    `,
+    note: `
+Harry actually does decapitate all the Death Eaters {114/(Black
+robes, falling.)}, but in more of a semi-circle than a line.
+
+TODO: backref
+    `,
+  },
+
+  {
+    tags: ['consequence', 'TODO'],
+    text: `
+what you did this morning to Neville Longbottom
+    `,
+    note: `
+TODO: backref
+    `,
+  },
+
+  {
+    tags: ['consequence', 'TODO'],
+    text: `
+But the prophecy...
+    `,
+    note: `
+TODO: backref
+    `,
+  },
+
+  {
+    tags: ['departure', 'TODO'],
+    text: `
+Apparently there were safeguards to prevent you from saying anything
+out loud by accident, while you were under the Hat talking about
+things you would never tell another soul for the rest of your life.
+    `,
+    note: `
+Pretty sure this is different from the original.  TODO: cross-check
+    `,
+  },
+
+  {
+    tags: ['spoiler', 'foreshadowing', 'TODO'],
+    text: `
+House of Doom
+    `,
+    note: `
+Harry later generals the Chaos Legion, whose battle cry is "DOOM",
+chanted to the tune of the Imperial March (TODO: reference).
+    `,
+  },
+
+  {
+    tags: ['consequence'],
+    text: `
+Harry gave a mental nod. To himself, he seemed pretty normal - just
+responding to the situations in which he found himself, that was
+all. But Professor McGonagall seemed to think that there was more to
+it than that.
+    `,
+    note: `
+Callback to the fundamental attribution error {5/fundamental
+attribution error}.
+    `,
+  },
+
+  {
+    tags: ['spoiler'],
+    text: `
+"That you don't like yourself when you're angry. That it is like
+wielding a sword whose hilt is sharp enough to draw blood from your
+hand, or looking at the world through a monocle of ice that freezes
+your eye even as it sharpens your vision."
+    `,
+    note: `
+**mysterious dark side**
+    `,
+  },
+
+  {
+    tags: ['spoiler'],
+    text: `
+"I cannot give you exact statistics. I cannot know them so I cannot
+count them. I just know that your chances don't feel good. They feel
+very not-good."
+    `,
+    note: `
+It's possible this is affected by Harry's **mysterious dark side**.
+    `,
+  },
+
+  {
+    tags: ['spoiler'],
+    text: `
+If you could do it this very moment and no one would ever know, you
+would.
+    `,
+    note: `
+This is an interesting statemnt, because Harry ponders this question
+at some length later in the series {85/batman} and eventually comes
+to the conclusion that he will at least try to achieve all of his
+goals without killing anyone, so long as nobody dies because of his
+reluctance.
+    `,
+  },
+];
