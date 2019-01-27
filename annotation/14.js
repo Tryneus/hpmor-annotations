d782f716b8543e6c2281fe69af21efbaabeaa709
Grey <grey@vex.local>
2019-01-27 01:37:39 -0500

    three more chapters

--- annotation/14.js
+++ annotation/14.js
@@ -1,167 +1,163 @@
-```
+module.exports = [
+  {
+    tags: ['foreshadowing', 'spoiler'],
+    text: `
 The Unknown and the Unknowable
-```
-
-* title
-* spoiler
-
+    `,
+    note: `
 A reference to the fact that later in the chapter Harry decides that
 the Universe might operate on principles which make it literally
 impossible for his brain to understand certain things.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 Melenkurion abatha! Duroc minas mill J. K. Rowling!
-```
-
-* note
-* reference
-* link
-
+    `,
+    note: `
 A reference to the Seven Words of Power from The Chronicles of Thomas
 Covenant, with J.K. Rowling replacing Harad and Khabaal.
 
 http://unbeliever.wikia.com/wiki/Seven_Words_of_Power
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler', 'foreshadowing'],
+    text: `
 There were mysterious questions, but a mysterious answer was a contradiction in terms.
-```
-
-* title-quote
-* spoiler
-
+    `,
+    note: `
 A flash-forward to later in the chapter.
 
 This might be here as a reminder of the Author's note from the
-previous chapter (13/`I solemnly swear that there is a logical,
+previous chapter {13/I solemnly swear that there is a logical,
 foreshadowed, canon-compliant explanation for everything which happens
-in this chapter.`).
+in this chapter.}.
 
 It might also be a reference to the fact that later in the chapter
 Harry decides that the Universe might operate on principles which make
 it literally impossible for his brain to understand certain things.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler'],
+    text: `
 It was at this point that Harry realised he was faced with a priceless
 and possibly irreplaceable opportunity to offer Professor McGonagall a
 Comed-Tea and he couldn't believe he was seriously thinking that
-```
-
-* spoiler
-
+    `,
+    note: `
 The **Comed-Tea** is affecting his brain because he is about to
 surprise McGonagall.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['speculation'],
+    text: `
 sounded a lot older than Latin
-```
-
-* speculation
-* spoiler
-
+    `,
+    note: `
 Possibly from Atlantis?
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['consequence'],
+    text: `
 Oh, right, she's expecting me to blackmail her for information about
 the prophecy.
-```
+    `,
+    note: `
+{6/You can, if you like, tell me the truth, the whole truth, and in
+return I will keep your secrets.}
+    `,
+  },
 
-* backref
-
-(6/`You can, if you like, tell me the truth, the whole truth, and in
-return I will keep your secrets.`)
-
----
-
-```
+  {
+    tags: ['consequence'],
+    text: `
 "It's about the Incident with the Sorting Hat,"
-```
+    `,
+    note: `
+{12/he'd heard a tiny whisper}
+    `,
+  },
 
-* backref
-
-(12/`he'd heard a tiny whisper`)
-
----
-
-```
+  {
+    tags: ['original'],
+    text: `
 "Well, yes, of course," Harry said. There was no need to admit how
 long it had taken him to actually think of that. "As opposed to, say,
 trying to research it myself, or telling any of the other children."
-```
-
-* original
-
+    `,
+    note: `
 This is poking fun at the original series - the other path sounds more
 typical of the main characters' antics.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['original'],
+    text: `
 "And if, perhaps, you were to discover the entrance to Salazar
 Slytherin's legendary Chamber of Secrets, an entrance that you and you
 alone could open..."
-```
-
-* original
-
+    `,
+    note: `
 An even more direct reference to the original series, in which Harry
 and Ron discover the chamber and explore it themselves, without
 bringing it to the attention of the school administrators.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['original'],
+    text: `
 "It's obvious if you're not a Gryffindor," Harry said kindly.
-```
-
-* original
-
+    `,
+    note: `
 An explanation on why Harry is acting so differently here - implying
 that Gryffindors are rather stupidly heroic.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['background'],
+    text: `
 there was a sharp intake of breath from McGonagall
-```
-
-* original
-
+    `,
+    note: `
 She recognizes it as parseltongue.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['original', 'background'],
+    text: `
 "Mr. Potter," McGonagall said, and now her voice was low, "five
 decades ago was the last time a student died within the walls of
 Hogwarts, and I am now certain that five decades ago was the last time
 someone heard that message."
-```
-
-* original
-
+    `,
+    note: `
 Voldemort opened the chamber of secrets 5 decades ago, and Slytherin's
 monster killed a student.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 Apparently that business with refusing offers of monetary rewards
 until you got a magic item actually worked in real life, not just
 computer games.
-```
-
-* reference
-
+    `,
+    note: `
 In many computer games, particularly RPGs, upon completing a quest the
 player is usually given a reward.  If the player refuses that reward,
 the quest-giver will usually offer a different means of compensation.
@@ -169,228 +165,206 @@ From a balance perspective, this is typically done so a player doesn't
 feel discouraged from playing a certain type of character.  In a
 similar way, McGonagall is trying to find a way to encourage Harry to
 be good by rewarding this behavior.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['original'],
+    text: `
 It's not fair to the other children if I earn lots of points for
 grownup things that I can be part of and they can't. How is Terry Boot
 supposed to earn fifty points for reporting a whisper he heard from
 the Sorting Hat? It wouldn't be fair at all.
-```
-
-* original
-
+    `,
+    note: `
 A little bit of a dig at the original series.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 No need to thank me, ma'am, virtue is its own reward.
-```
-
-* reference
-
+    `,
+    note: `
 A mashup of a common Superhero saying with a quote from John Henry
 Newman.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['background'],
+    text: `
 Spontaneous Duplication
-```
-
-* subtle
-
+    `,
+    note: `
 Spontaneous duplication is also a convenient excuse if someone sees
 your past self and future self in the same place at the same time.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 Gutenberg
-```
-
-* reference
-* link
-
+    `,
+    note: `
 Johannes Gutenberg was arguably the proximate cause of Printing
 spreading across Europe, which arguably led directly to the
 enlightenment.
 
 https://en.wikipedia.org/wiki/Johannes_Gutenberg
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['original'],
+    text: `
 "Perhaps," Harry said when he could speak again, "you ought to provide
 your time machines with some sort of protective shell, rather than
 leaving the glass exposed, so as to prevent that from happening."
-```
-
-* original
-
+    `,
+    note: `
 While this serves to show the lack of critical thinking in the
 wizarding world, it is also a jab at the source material.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 ANTHROPIC PRINCIPLE
-```
-
-* reference
-* link
-
+    `,
+    note: `
 Nobody can ever observe living in a Universe that has been destroyed,
 so observing that that Universe hasn't been destroyed by something yet
 isn't the strong evidence that you might think it is.
 
 https://en.wikipedia.org/wiki/Anthropic_principle
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['consequence'],
+    text: `
 You're having another 'you turned into a cat' moment
-```
-
-* backref
-
+    `,
+    note: `
 (2/`Professor McGonagall turned into a cat.`)
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 Simulacron 3
-```
-
-* link
-
+    `,
+    note: `
 https://en.wikipedia.org/wiki/Simulacron-3
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 Turing machine
-```
-
-* reference
-* link
-
+    `,
+    note: `
 A turing machine is a very simple machine which turns out to be
 equivalent in computing power to every machine anyone has been able to
 conceive of, given enough time to compute.
 
 https://en.wikipedia.org/wiki/Turing_machine
+    `,
+  },
 
----
-
-```
-"SO THAT'S HOW THE COMED-TEA WORKS!
-```
-
-* reveal
-
-**comed-tea**
-
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 CAUSAL ARROWS
-```
-
-* reference
-* link
-
+    `,
+    note: `
 https://en.wikipedia.org/wiki/Arrow_of_time#The_causal_arrow_of_time
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler', 'original'],
+    text: `
 said something about not talking to snakes where anyone could hear him
-```
-
-* spoiler
-* original
-
+    `,
+    note: `
 Harry is a parseltongue, like in the original, and McGonagall realizes
 this, but Harry entirely fails to pick up on her meaning.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 E. T. Jaynes
-```
-
-* reference
-* link
-
+    `,
+    note: `
 https://en.wikipedia.org/wiki/Edwin_Thompson_Jaynes
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 that a blank map did not correspond to a blank territory
-```
-
-* reference
-* link
-
+    `,
+    note: `
 https://en.wikipedia.org/wiki/Map%E2%80%93territory_relation
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 Lord Kelvin
-```
-
-* reference
-* link
-
+    `,
+    note: `
 https://en.wikipedia.org/wiki/William_Thomson,_1st_Baron_Kelvin
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['departure'],
+    text: `
 The perfect, stereotypical image of a Christmas present, although it
 wasn't Christmas.
-```
-
-* original
-* spoiler
-
+    `,
+    note: `
 This gift was from Dumbledore in the original series, and given to
 Harry in Christmas of his first year.  It is likely that this was
 accelerated like many other things in this series.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['speculation'],
+    text: `
 It seems to expect you to have need of it. Use it well.
-```
-
-* speculation
-
+    `,
+    note: `
 It's possible that the cloak did, in fact, know that Harry would need
 it to play the prank on himself.  HPMOR has fewer coincidences than
 most stories.
+    `,
+  },
 
----
-
----
-
-```
+  {
+    tags: ['spoiler', 'original'],
+    text: `
 If Dumbledore saw a chance to possess one of the Deathly Hallows, he
 would never let it escape his grasp until the day he died.
-```
-
-* spoiler
-* original
-
+    `,
+    note: `
 The cloak was left by Dumbledore, just like in the original, probably
-as part of his **prophecy-threading** project (79/`"Albus," Minerva
+as part of his **prophecy-threading** project {79/"Albus," Minerva
 said, surprised at how steady her own voice was, "did you leave those
-notes under Mr. Potter's pillow?"`).
+notes under Mr. Potter's pillow?"}.
 
 Dumbledore also posesses the Elder Wand, another Deathly Hallow, which
 changes hands when its owner is defeated, an event which Dumbledore
@@ -399,66 +373,69 @@ expects to coincide with his death.
 Shortly after Harry receives the cloak, Dumbledore will conspicuously
 *not* steal it, even though he has the opportunity to, and will tell
 Harry that Harry should be more suspicious next time somebody says not
-to trust him (17/`I will not hold your caution against you, Harry, we
+to trust him {17/I will not hold your caution against you, Harry, we
 must all do our best to be wise. I will only ask that you think twice
 and ponder three times again, the next time someone tells you to
-distrust me.`).  Assuming that he didn't write the note this way just
+distrust me.}.  Assuming that he didn't write the note this way just
 because the prophecies told him to, it seems likely that he did it in
 order to make Harry trust him more by proving a strawman wrong.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['original', 'background'],
+    text: `
 Cloak of Invisibility of Ignotus Peverell
-```
-
-* original
-* spoiler
-
+    `,
+    note: `
 See **deathly hallows** topic page.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler', 'departure'],
+    text: `
 hidden, not merely invisible
-```
-
-* spoiler
-
+    `,
+    note: `
 This seems to manifest as the cloak hiding you from all forms of
-perception, magical or otherwise.
+perception, magical or otherwise.  In the original series, forms
+of magical things could detect a cloaked presence, such as Alastor
+Moody's eye and Dementors.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['consequence'],
+    text: `
 Um, shouldn't you tell students all that sort of stuff right away?
-```
-
-* subtle
-
+    `,
+    note: `
 Harry notably did not have this advice when navigating the castle
 earlier in his timeline.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 Oh wad some power the giftie gie us
 To see oursel's as others see us!
 It wad frae monie a blunder free us,
 And foolish notion -
-```
-
-* reference
-* link
-
+    `,
+    note: `
 https://en.wikipedia.org/wiki/To_a_Louse
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler'],
+    text: `
 There was something wrong with Harry Potter.
-```
-
-* spoiler
-
+    `,
+    note: `
 **mysterious dark side**
+    `,
+  },
+];
--- annotation/15.js
+++ annotation/15.js
@@ -1,199 +1,193 @@
-```
+module.exports = [
+  {
+    tags: ['reference'],
+    text: `
 Conscientiousness
-```
-
-* title
-* reference
-* link
-
+    `,
+    note: `
 One of the big five personality traits (a psychological model).  Turns
 out to predict a lot of important things.
 
 https://en.wikipedia.org/wiki/Big_Five_personality_traits#Conscientiousness
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 Love as thou Rowling.
-```
-
-* note
-* reference
-* link
-
+    `,
+    note: `
 A reference to the Kushiel's Legacy saga by Jacqueline Carey.  The
 deity Elau has a precept, "Love as thou wilt."
 
 https://en.wikipedia.org/wiki/Kushiel%27s_Dart
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['foreshadowing', 'spoiler'],
+    text: `
 "I'm sure I'll find the time somewhere."
-```
-
-* title-quote
-
+    `,
+    note: `
 A flash-forward to later in the chapter.  Pretty obviously a reference
 to the **Time-Turner** Harry has acquired.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['background'],
+    text: `
 And it was starting to look like he had a mysterious dark side.
-```
-
+    `,
+    note: `
 This is the first time it is explicitly called a **mysterious dark
 side**.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['consequence'],
+    text: `
 Thought about the Slytherins bullying Neville, and the game where
 someone knocked down your books every time you tried to pick them up
 again.
-```
+    `,
+    note: `
+{7/Harry's eyes dropped downwards}
+    `,
+  },
 
-* backref
-
-(7/`Harry's eyes dropped downwards`)
-
----
-
-```
+  {
+    tags: ['consequence'],
+    text: `
 Thought about what Draco Malfoy had said about the ten-year-old
 Lovegood girl and how the Wizengamot really operated...
-```
+    `,
+    note: `
+{7/As soon as I'm old enough I'm going to rape her}
+    `,
+  },
 
-* backref
-
-(7/`As soon as I'm old enough I'm going to rape her`)
-
----
-
-```
+  {
+    tags: ['consequence'],
+    text: `
 how the Wizengamot really operated
-```
+    `,
+    note: `
+{7/Um... seat on the Wizengamot}
+    `,
+  },
 
-* backref
-
-(7/`Um... seat on the Wizengamot`)
-
----
-
-```
+  {
+    // TODO: spoiler and non-spoiler in the same annotation
+    tags: ['reference', 'TODO'],
+    text: `
 He wanted to write someone and demand a refund on his dark side which
 clearly ought to have irresistible magical power but had turned out to
 be defective.
-```
-
-* link
-
+    `,
+    note: `
 There are various overlapping tropes for this, but it is common
 enough: http://tvtropes.org/pmwiki/pmwiki.php/Main/SuperpoweredEvilSide
 
-* spoiler
-
+Spoiler:
 As it turns out, Harry is already using his mysterious dark side, it
 is the reason he is not like other kids and why he has excelled so
 much in his life to this point.
+    `,
+  },
 
----
-
-```
+  {
+    // TODO: I have a suspicion that Harry breaks every one of the vows
+    // sworn in this chapter - investigate
+    tags: ['foreshadowing', 'TODO', 'original'],
+    text: `
 "Transfiguration is some of the most complex and dangerous magic you
 will learn at Hogwarts," said Professor McGonagall. There was no trace
 of any levity upon the face of the stern old witch. "Anyone messing
 around in my class will leave and not come back. You have been
 warned."
-```
-
-* original
-* foreshadowing
-
+    `,
+    note: `
 While this is the exact same intro given by McGonagall in the original
 series, the rest of this scene establishes much more hard and fast
 rules on Transfiguration, with more of a scientific basis.  This
 foreshadows the importance that Transfiguration will play in this
 series.
+    `,
+  },
 
----
-
-TODO: I have a suspicion that Harry breaks every one of the vows
-sworn in this chapter - investigate
-
----
-
-```
+  {
+    tags: ['foreshadowing', 'spoiler'],
+    text: `
 "You needn't apologise, Mr. Potter, if you were required to read ahead
 you would have been told to do so."
-```
-
-* spoiler
-* foreshadowing
-
-Snape, of course, is much less reasonable (18/`"Potter!" snapped the
+    `,
+    note: `
+Snape, of course, is much less reasonable {18/"Potter!" snapped the
 Potions professor. "What would I get if I added powdered root of
-asphodel to an infusion of wormwood?"`).
+asphodel to an infusion of wormwood?"}.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 Harry spoke first. "Of course you realise this means war."
-```
-
-* reference
-* link
-
+    `,
+    note: `
 A bit of hyperbole, but a common trope:
 http://tvtropes.org/pmwiki/pmwiki.php/Main/ThisMeansWar
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['consequence'],
+    text: `
 your suggestion for preventing breakage in her Spimster wickets
-```
+    `,
+    note: `
+{14/some sort of protective shell}
+    `,
+  },
 
-* backref
-
-(14/`some sort of protective shell`)
-
----
-
-```
+  {
+    tags: ['spoiler'],
+    text: `
 scales with the size of the target form
-```
-
-* subtle
-* spoiler
-
+    `,
+    note: `
 Scales with the size of the *target* form.  Harry later uses this to
 permanently maintain a transfiguration that maps a large object onto a
 very small object.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['spoiler', 'foreshadowing']
+    text: `
 Unless, of course, you engage in some sort of mischief and lose House
 points before then. Addressing a professor disrespectfully, for
 example." Professor McGonagall put a finger on her cheek and looked
 reflective. "I expect you'll hit negative numbers before the end of
 Friday.
-```
-
-* spoiler
-* foreshadowing
-
+    `,
+    note: `
 Harry does, in fact, lose a great number of points for addressing
-Snape disrespectfully before the end of Friday (18/`Ten more points
-from Ravenclaw.`).
+Snape disrespectfully before the end of Friday {18/Ten more points
+from Ravenclaw.}.
+    `,
+  },
 
----
-
-```
+  {
+    tags: ['reference'],
+    text: `
 And there was evening and there was morning, the first day.
-```
-
-* reference
-
+    `,
+    note: `
 A reference to Genesis 1:5.
+    `,
+  },
+];
